730293375d6634e1332610eff42e335845439e60
spk-2005 <prasannasimha5002@gmail.com>
2025-03-03 07:00:03 +0530

    first commit

--- server.js
+++ server.js
@@ -20,10 +20,7 @@ app.use("/api/todos", todoRoutes);
 /////
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.error(err));
 
