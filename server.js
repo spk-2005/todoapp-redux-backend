6265e97821d3c5fa41321fd281430f6290dab423
spk-2005 <prasannasimha5002@gmail.com>
2025-03-03 07:41:07 +0530

    first commit

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.error(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
