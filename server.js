688dd150b0fd2dd7434a61964e94b3761842d578
spk-2005 <prasannasimha5002@gmail.com>
2025-03-03 07:50:50 +0530

    first commit

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // Use environment provided port or fallback to 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
